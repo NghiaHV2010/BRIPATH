bd0245f1d24ca4ffa021db0b6d5308efff69e60a
Van Nia <hvnia2010@gmail.com>
2025-09-22 00:19:30 +0700

    Init database orm & auth middleware

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -3,13 +3,19 @@ import dotenv from "dotenv";
 interface Config {
     PORT: number;
     FRONTEND_URL: string;
+    DATABASE_URL: string;
+    ACCESS_SECRET: string;
+    REFRESH_SECRET: string;
 }
 
 dotenv.config();
 
 const config: Config = {
     PORT: Number(process.env.PORT)!,
-    FRONTEND_URL: process.env.FRONTEND_URL!
+    FRONTEND_URL: process.env.FRONTEND_URL!,
+    DATABASE_URL: process.env.DATABASE_URL!,
+    ACCESS_SECRET: process.env.ACCESS_SECRET!,
+    REFRESH_SECRET: process.env.REFRESH_SECRET!,
 };
 
 export default config;
---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,78 @@
+import { NextFunction, Request, Response } from "express";
+import { errorHandler } from "../utils/error";
+import { HTTP_ERROR } from "../constants/httpCode";
+import jwt from "jsonwebtoken";
+import config from "../config/env.config";
+import { PrismaClient } from "../generated/prisma";
+
+const { ACCESS_SECRET, REFRESH_SECRET } = config;
+
+const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const prisma = new PrismaClient();
+
+    let newAccessToken: string;
+    let userId: string;
+
+    try {
+        const accessToken: string = req.cookies?.accessToken;
+
+        if (!accessToken) {
+            const refreshToken: string = req.cookies?.refreshToken;
+
+            if (!refreshToken) {
+                return next(errorHandler(HTTP_ERROR.UNAUTHORIZED, "Unauthorized - Please login!"));
+            }
+
+            const refreshTokenDecoded = jwt.verify(refreshToken, REFRESH_SECRET);
+
+            if (!refreshTokenDecoded) {
+                return next(errorHandler(HTTP_ERROR.UNAUTHORIZED, "Unauthorized - Invalid token!"));
+            }
+
+            //@ts-ignore
+            userId = refreshToken.userId;
+
+            newAccessToken = jwt.sign({ userId }, ACCESS_SECRET, {
+                expiresIn: "45m"
+            });
+
+            res.cookie("accessToken", newAccessToken, {
+                maxAge: 45 * 60 * 1000,
+                httpOnly: true,
+                sameSite: "strict",
+                secure: false //true when https - false when http
+            });
+        } else {
+            const accessTokenDecoded = jwt.verify(accessToken, ACCESS_SECRET);
+
+            if (!accessTokenDecoded) {
+                return next(errorHandler(HTTP_ERROR.UNAUTHORIZED, "Unauthorized - Invalid token!"));
+            }
+
+            //@ts-ignore
+            userId = accessTokenDecoded.userId;
+        }
+
+        const user = await prisma.users.findFirst({
+            where: {
+                id: userId
+            },
+            omit: {
+                password: true
+            }
+        });
+
+        if (!user) {
+            next(errorHandler(HTTP_ERROR.NOT_FOUND, "User not found!"));
+        }
+
+        //@ts-ignore
+        req.user = user;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default authMiddleware;
