ccebc8f198c702e96917a6aaf107e93f124ca357
DeePham <dungphse184328@fpt.edu.vn>
2025-09-23 14:22:10 +0700

    [EnvConfig]Update

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -33,7 +33,7 @@ const config: Config = {
     VNPAY_TMN_CODE: process.env.VNPAY_TMN_CODE || '',
     VNPAY_HASH_SECRET: process.env.VNPAY_HASH_SECRET || '',
     VNPAY_URL: process.env.VNPAY_URL || 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
-    VNPAY_API: process.env.VNPAY_API || 'https://sandbox.vnpayment.vn/merchant_webapi/api/transaction', 
+    VNPAY_API: process.env.VNPAY_API || 'https://sandbox.vnpayment.vn/merchant_webapi/api/transaction',
     VNPAY_RETURN_URL: process.env.VNPAY_RETURN_URL || 'http://localhost:3000/api/vnpay/return',
     DATABASE_URL: process.env.DATABASE_URL!,
     ACCESS_SECRET: process.env.ACCESS_SECRET!,
@@ -42,5 +42,22 @@ const config: Config = {
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!
 };
 
-export default config;
-
+export const {
+    PORT,
+    FRONTEND_URL,
+    ZALOPAY_APP_ID,
+    ZALOPAY_KEY1,
+    ZALOPAY_KEY2,
+    ZALOPAY_ENDPOINT,
+    ZALOPAY_CALLBACK_URL,
+    VNPAY_TMN_CODE,
+    VNPAY_HASH_SECRET,
+    VNPAY_URL,
+    VNPAY_API,
+    VNPAY_RETURN_URL,
+    DATABASE_URL,
+    ACCESS_SECRET,
+    REFRESH_SECRET,
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET
+} = config;
