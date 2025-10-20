2f18ee118e301a5cb191a8383a5abb0b13dbcf99
DeePham <dungphse184328@fpt.edu.vn>
2025-10-21 00:52:16 +0700

    [Fix]Fix error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   css: {
-    transformer: 'postcss', // dùng postcss thay vì lightningcss
+    postcss: './postcss.config.js',
+  },
+  build: {
+    rollupOptions: {
+      external: ['lightningcss'],
+    },
   },
 })
