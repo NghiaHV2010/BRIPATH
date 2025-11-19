9f9f6ff474ef14a3b006dee9891b4255dce65efc
Van Nia <hvnia2010@gmail.com>
2025-11-20 01:29:05 +0700

    fix error

--- backend/src/libs/prisma.ts
+++ backend/src/libs/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { DATABASE_URL, NODE_ENV } from '../config/env.config';
+import { NODE_ENV } from '../config/env.config';
 
 // Khai báo một biến toàn cục để lưu trữ instance
 const globalForPrisma = globalThis as unknown as {
@@ -12,11 +12,6 @@ export const prisma =
     new PrismaClient({
         // (Tùy chọn) Bật log để xem các query khi ở môi trường dev
         // log: ['query', 'info', 'warn', 'error'],
-        datasources: {
-            db: {
-                url: DATABASE_URL, // Truyền trực tiếp chuỗi kết nối vào đây
-            },
-        },
     });
 
 // Đây là một mẹo để tránh tạo nhiều instance khi
